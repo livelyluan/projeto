c4576fae752dbd028439f1fb5357ab793024e366
livelyluan <luandaniel13gremista@gmail.com>
2024-06-21 19:49:24 -0300

    Add files via upload

---
+++ custom_appbar.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+AppBar custom_appbar(String title, double height,Color background) 
+{
+  return AppBar(
+  title:Text(title),
+  toolbarHeight: height,
+  backgroundColor: background,
+  actions: const [
+   SearchBar(),
+   ], 
+  );
+}
